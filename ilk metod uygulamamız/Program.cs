5c2ec4c6bc34a5b9d1f93be08fb98e537e09a431
cansugunduzalp <cansugunduzalp@gmail.com>
2024-12-31 01:30:23 +0300

    Add project files.

---
+++ ilk metod uygulamamız/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        // Geriye değer döndürmeyen void bir metot
+        static void HalimizVeKeyfimiz()
+        {
+            // "Halimiz itten beter keyfimiz paşada yok" ve "Koy masaya bir duble buzsuz olsun" cümlelerini yazdırma
+            Console.WriteLine("Halimiz itten beter, keyfimiz paşada yok.");
+            Console.WriteLine("Koy masaya bir duble buzsuz olsun.");
+        }
+
+        static void Main(string[] args)
+        {
+            // HalimizVeKeyfimiz metodunu çağırıyoruz
+            HalimizVeKeyfimiz();
+
+            // Kullanıcının bir tuşa basmasını bekliyoruz
+            Console.ReadKey();
+        }
+    }
+}
